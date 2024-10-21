9193f2be7e4c16060b4d779e2f5e1254c89c91a6
Jefsaenz97 <jeferson.valeta@dentsu.com>
2024-10-21 00:34:13 -0500

    firts commit v2

--- src/database/database.js
+++ src/database/database.js
@@ -16,7 +16,7 @@ export const sequelize = new Sequelize(
 export async function connectDB() {
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('Connection has been established successfully!.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
